793e01f966a483924e1407f2e71d4d226001907c
Maria Tarliuk <m.tarliuk@mail.ru>
2020-09-29 14:03:07 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
+      if(!arguments.length || array .length ==0) return 0
+   let i=0;
+   let min = array[0]
+   while(i<array.length) {
+      if(min>array[i]) min = array[i];
+      i++
+   }
+   return min
 }
 
 exports.max = function max (array) {
-  return 0;
+   if(!arguments.length || array .length ==0) return 0
+   let arr = array.sort((a,b)=> b-a)
+return arr[0]
 }
 
 exports.avg = function avg (array) {
-  return 0;
+   if(!arguments.length || array.length ==0) return 0;
+   return array.reduce((a,b)=> a+b, 0)/array.length
 }
